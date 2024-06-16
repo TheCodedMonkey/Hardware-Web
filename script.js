a86612dbcc08adb318b6663036cfacdb865be731
TheCodedMonkey <165838108+TheCodedMonkey@users.noreply.github.com>
2024-06-16 12:53:31 -0700

    V2

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to handle button click event
+document.getElementById("startButton").addEventListener("click", function() {
+    window.location.href = "newpage.html"; // Replace with your desired URL
+  });
